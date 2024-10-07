b65678bcb38199ed590d861acc56152c0dbf0952
Fedechkin Aleksei <alex@centralhardware.me>
2024-10-07 21:41:46 +0200

    add default value for DEBUG

--- src/main/kotlin/dev/inmo/kslog/common/KSLogConfiguration.kt
+++ src/main/kotlin/dev/inmo/kslog/common/KSLogConfiguration.kt
@@ -3,7 +3,7 @@ package dev.inmo.kslog.common
 import org.apache.commons.lang3.BooleanUtils
 
 fun KSLog.configure(appName: String) {
-    val minLogLevel = if (BooleanUtils.toBooleanObject(System.getenv("DEBUG"))) {
+    val minLogLevel = if (BooleanUtils.toBooleanObject(System.getenv("DEBUG") ?: "false")) {
         LogLevel.DEBUG
     } else {
         LogLevel.INFO
