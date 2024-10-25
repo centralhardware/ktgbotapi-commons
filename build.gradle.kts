73ccf11a6baf5c157186fac98713b1a6bff82978
Fedechkin Aleksei <alex@centralhardware.me>
2024-10-25 08:04:35 +0200

    bump logging middleware

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 
     implementation("dev.inmo:kslog:1.3.6")
     implementation("dev.inmo:tgbotapi:18.2.2")
-    implementation("com.github.centralhardware:ktgbotapi-clickhouse-logging-middleware:17c3019f89")
+    implementation("com.github.centralhardware:ktgbotapi-clickhouse-logging-middleware:1117274ee1")
 }
 
 tasks.test {
