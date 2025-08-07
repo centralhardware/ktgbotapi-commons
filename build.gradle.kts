c70962ad979e10b86b7313a4f2eb23d33a017dad
alex <alex@centralhardware.me>
2025-08-07 14:17:59 +0800

    refactor dependency versioning for ktgbotapi and associated middleware

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,15 @@ repositories {
     maven("https://jitpack.io")
 }
 
+val ktgbotapiVersion = "27.1.2"
+
 dependencies {
     implementation("org.apache.commons:commons-lang3:3.18.0")
 
     implementation("dev.inmo:kslog:1.5.0")
-    implementation("dev.inmo:tgbotapi:27.1.2")
-    implementation("com.github.centralhardware:ktgbotapi-clickhouse-logging-middleware:50869a92bf")
-    implementation("com.github.centralhardware:ktgbotapi-stdout-logging-middleware:704657ed0d")
+    implementation("dev.inmo:tgbotapi:$ktgbotapiVersion")
+    implementation("com.github.centralhardware:ktgbotapi-clickhouse-logging-middleware:$ktgbotapiVersion")
+    implementation("com.github.centralhardware:ktgbotapi-stdout-logging-middleware:$ktgbotapiVersion")
 }
 
 tasks.test {
