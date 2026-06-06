e9517e98385b1e91e45d60c09982666b89edc5b4
alex <alex@centralhardware.me>
2026-06-06 09:37:58 +0800

    fix access to ktgbotapi

--- ktgbotapi-commons-core/build.gradle.kts
+++ ktgbotapi-commons-core/build.gradle.kts
@@ -9,10 +9,10 @@ group = "me.centralhardware"
 val ktgbotapiVersion = "33.1.0"
 
 dependencies {
-    api("org.apache.commons:commons-lang3:3.20.0")
+    implementation("org.apache.commons:commons-lang3:3.20.0")
 
-    implementation("dev.inmo:kslog:1.6.1")
-    implementation("dev.inmo:tgbotapi:$ktgbotapiVersion")
+    api("dev.inmo:kslog:1.6.1")
+    api("dev.inmo:tgbotapi:$ktgbotapiVersion")
     api(project(":ktgbotapi-conversation"))
     api(project(":ktgbotapi-stdout-logging-middleware"))
     api(project(":ktgbotapi-clickhouse-logging-middleware"))
